34e2015c958f6ce89c0c106775e50dffa2f30a95
西下 慧 <Nsatoshi@saika-no-iMac.local>
2014-12-01 20:42:24 +0900

    2014/12/01/21:00

---
+++ dbconnect.php
@@ -0,0 +1,13 @@
+<?php
+//データベース接続用のphpファイル
+//$db = mysql_connect('mysql016.phy.lolipop.lan', 'LAA0374382', '451mdg758', 'LAA0374382-kiso') or die(mysqli_connect_error());
+//mysql_query("SET NAMES utf8");
+//MySQLサーバ名
+	$DBSERVER = "mysql016.phy.lolipop.lan";
+	//ログインユーザ名
+	$DBUSER = "LAA0374382";
+	//パスワード
+	$DBPASSWORD = "451mdg758";
+	//データベース名
+	$DBNAME = "LAA0374382-kiso";
+?>
